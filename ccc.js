a9aef5f7b4a16a4d513b1cad5b40871488720ae9
Mark Murphy <mark.murphy@techemail.com>
2020-11-01 15:59:43 +0000

    Added openAfterExport option

--- ccc.js
+++ ccc.js
@@ -409,10 +409,15 @@ try {
                                         console.log(chalk.grey('Results written to [%s]'), filename);
                                         if (settings.options.openAfterExport) {
                                             debug('Opening [%s] ...', filename);
+
+                                            let open = require('open');
+
+                                            (async () => {
+                                                // Opens the image in the default image viewer and waits for the opened app to quit.
+                                                await open(filename);
+                                            })();
                                         }
                                     });
-
-
                                 }
                             });
                         }

ac82799138ab437b01b2ceb232f85d664a01e0f6
Mark Murphy <mark.murphy@techemail.com>
2020-11-25 19:39:19 +0000

    Changed --help --verbose to a more pleasing blue

--- help.js
+++ help.js
@@ -11,7 +11,7 @@ module.exports = {
         const package = require('./package.json');
 
         // Display help screen
-        console.log(chalk.blue(package.name));
+        console.log(chalk.blueBright(package.name));
         console.log(chalk.green('Read the docs: ') + package.homepage);
         console.log(chalk.magenta('Support & bugs: ') + package.bugs.url);
         console.log(endOfLine);
@@ -48,21 +48,21 @@ module.exports = {
             const utils = require('./utils');
             console.log(endOfLine);
             console.log(chalk.grey('SYSTEM:'));
-            console.log('   Hostname           ' + chalk.blue(os.hostname()));
-            console.log('   Uptime             ' + chalk.blue(utils.secondsToHms(os.uptime())));
-            console.log('   Platform           ' + chalk.blue(os.platform()));
-            console.log('   O/S                ' + chalk.blue(os.type()));
-            console.log('   O/S release        ' + chalk.blue(os.release()));
-            console.log('   CPU architecture   ' + chalk.blue(os.arch()));
-            console.log('   CPU cores          ' + chalk.blue(os.cpus().length));
-            console.log('   CPU model          ' + chalk.blue(os.cpus()[0].model));
-            console.log('   Free memory        ' + chalk.blue(utils.formatBytes(os.freemem())));
-            console.log('   Total memory       ' + chalk.blue(utils.formatBytes(os.totalmem())));
-            console.log('   Home directory     ' + chalk.blue(os.homedir()));
-            console.log('   Temp directory     ' + chalk.blue(os.tmpdir()));
-            console.log('   Console width      ' + chalk.blue(process.stdout.columns));
-            console.log('   Console height     ' + chalk.blue(process.stdout.rows));
-            console.log('   Colour support     ' + chalk.blue(utils.getColourLevelDesc()));
+            console.log('   Hostname           ' + chalk.blueBright(os.hostname()));
+            console.log('   Uptime             ' + chalk.blueBright(utils.secondsToHms(os.uptime())));
+            console.log('   Platform           ' + chalk.blueBright(os.platform()));
+            console.log('   O/S                ' + chalk.blueBright(os.type()));
+            console.log('   O/S release        ' + chalk.blueBright(os.release()));
+            console.log('   CPU architecture   ' + chalk.blueBright(os.arch()));
+            console.log('   CPU cores          ' + chalk.blueBright(os.cpus().length));
+            console.log('   CPU model          ' + chalk.blueBright(os.cpus()[0].model));
+            console.log('   Free memory        ' + chalk.blueBright(utils.formatBytes(os.freemem())));
+            console.log('   Total memory       ' + chalk.blueBright(utils.formatBytes(os.totalmem())));
+            console.log('   Home directory     ' + chalk.blueBright(os.homedir()));
+            console.log('   Temp directory     ' + chalk.blueBright(os.tmpdir()));
+            console.log('   Console width      ' + chalk.blueBright(process.stdout.columns));
+            console.log('   Console height     ' + chalk.blueBright(process.stdout.rows));
+            console.log('   Colour support     ' + chalk.blueBright(utils.getColourLevelDesc()));
         }
     }
 };

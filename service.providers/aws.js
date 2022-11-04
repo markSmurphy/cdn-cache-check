39e172de01d7c58a34e0645378e4e9ffa4f1af93
markSmurphy <mark.murphy@techemail.com>
2022-11-04 17:45:46 +0000

    Comment out redundent AWS code

--- service.providers/aws.js
+++ service.providers/aws.js
@@ -5,10 +5,10 @@ debug('Entry: [%s]', __filename);
 const IPCIDR = require('ip-cidr');
 
 // File System library
-const fs = require('fs');
+// const fs = require('fs');
 
 // Platform independent path separator
-const pathSeparator = require('path').sep;
+// const pathSeparator = require('path').sep;
 
 // Load the root service.providers configuration file
 const serviceProviders = require('./services.json');
@@ -16,63 +16,63 @@ const serviceProviders = require('./services.json');
 // Load the AWS IP Address and services data
 const services = require(serviceProviders.AWS.dataFile);
 
-module.exports = {
-    lookupIpAddress(ipAddress, options = { verbose: false }) {
-        debug('lookupIpAddress(%s)::entry', ipAddress, options);
 
-        // Initialise response object
-        let response = global.CCC_SERVICE_DETECTION_DEFAULT_RESPONSE;
-        response.ipAddress = ipAddress;
+function lookupIpAddress(ipAddress, options = { verbose: false }) {
+    debug('lookupIpAddress(%s)::entry', ipAddress, options);
 
-        // Loop through each service
-        debug('Checking if the IP address [%s] matches one of %s known Azure services', ipAddress, services.values.length);
-        for (let i = 0; i < services.values.length; i++) {
-            // Checking against each service
-            debug('Checking against %s CIDR blocks for service ID: %s', services.values[i].properties.addressPrefixes.length, services.values[i].id);
-            for (let ii = 0; ii < services.values[i].properties.addressPrefixes.length; ii++) {
-                // Checking within each CIDR block for the current service
-                let cidr = new IPCIDR(services.values[i].properties.addressPrefixes[ii]);
+    // Initialise response object
+    let response = global.CCC_SERVICE_DETECTION_DEFAULT_RESPONSE;
+    response.ipAddress = ipAddress;
 
-                // Check if the IP address exists within the cidr block
-                if (cidr.contains(ipAddress)) {
-                    response.reason = `${ipAddress} is in the CIDR block ${services.values[i].properties.addressPrefixes[ii]} which is used by Azure service: ${services.values[i].id}`;
-                    let strMessage = services.values[i].properties.systemService;
-                    // Check if there's a specific region within the data
-                    if (services.values[i].properties.regionId > 0) {
-                        // Record and append region
-                        response.regionId = services.values[i].properties.regionId;
-                        response.region = services.values[i].properties.region;
-                        strMessage += ` (${services.values[i].properties.region})`;
-                    }
-                    response.messages.push(strMessage);
-                    response.status = global.CCC_SERVICE_DETECTION_STATUS_LABEL.AZURE;
+    // Loop through each service
+    debug('Checking if the IP address [%s] matches one of %s known Azure services', ipAddress, services.values.length);
+    for (let i = 0; i < services.values.length; i++) {
+        // Checking against each service
+        debug('Checking against %s CIDR blocks for service ID: %s', services.values[i].properties.addressPrefixes.length, services.values[i].id);
+        for (let ii = 0; ii < services.values[i].properties.addressPrefixes.length; ii++) {
+            // Checking within each CIDR block for the current service
+            let cidr = new IPCIDR(services.values[i].properties.addressPrefixes[ii]);
 
-                    if (options.verbose === false) { // Check if verbose mode is disabled, because we'll log everything if it's not
+            // Check if the IP address exists within the cidr block
+            if (cidr.contains(ipAddress)) {
+                response.reason = `${ipAddress} is in the CIDR block ${services.values[i].properties.addressPrefixes[ii]} which is used by Azure service: ${services.values[i].id}`;
+                let strMessage = services.values[i].properties.systemService;
+                // Check if there's a specific region within the data
+                if (services.values[i].properties.regionId > 0) {
+                    // Record and append region
+                    response.regionId = services.values[i].properties.regionId;
+                    response.region = services.values[i].properties.region;
+                    strMessage += ` (${services.values[i].properties.region})`;
+                }
+                response.messages.push(strMessage);
+                response.status = global.CCC_SERVICE_DETECTION_STATUS_LABEL.AZURE;
 
-                        break; // Break out of 'ii' for-loop (around CIDR blocks) as we've found a match
+                if (options.verbose === false) { // Check if verbose mode is disabled, because we'll log everything if it's not
 
-                    }
+                    break; // Break out of 'ii' for-loop (around CIDR blocks) as we've found a match
 
                 }
-            }
 
-            if (options.verbose === false) { // Check if verbose mode is disabled, because we'll log everything if it's not
-                if (response.regionId > 0) {
-
-                    break; // Break out of 'i' for-loop (around Azure Services) as we've found a match with a specific region
-
-                }
             }
         }
 
-        // Concatenate messages into one response string
-        response.message = response.messages.join(' || ');
+        if (options.verbose === false) { // Check if verbose mode is disabled, because we'll log everything if it's not
+            if (response.regionId > 0) {
 
-        return (response);
+                break; // Break out of 'i' for-loop (around Azure Services) as we've found a match with a specific region
+
+            }
+        }
     }
-};
 
-function previousAWS() {
+    // Concatenate messages into one response string
+    response.message = response.messages.join(' || ');
+
+    return (response);
+}
+
+
+/* function previousAWS() {
     // Check the IP Address against the AWS service list
     let awsServicesFile = `${__dirname}${pathSeparator}service.providers/aws/ip-ranges.json`;
     let rawData = fs.readFileSync(awsServicesFile); // Read the AWS services file
@@ -103,4 +103,6 @@ function previousAWS() {
         // Save the generated message into the response object
         cdnResponse.message.push(`[${awsServicesMessage.join(' -> ')}]`);
     }
-}
+} */
+
+module.exports = { lookupIpAddress };
